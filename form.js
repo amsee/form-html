8000c8a25a43e0960bf121f13d38fa5900af7e34
Amy <amsee@users.noreply.github.com>
2020-01-20 12:18:20 -0500

    adding files

--- form.js
+++ form.js
@@ -1,4 +1,9 @@
 function formInfo() {
+    // check if function is being called
+    // console.log('Calling formInfo()');
     // x contains the value of the element that has an id of firstname
     let x = document.getElementById("firstname").value;
+    // display the value of firstname and email
+    console.log(firstname.value);
+    console.log(email.value);
 }
